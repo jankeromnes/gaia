e257146ade49f5aaf5b4a8e47e300cc6a0b633bd
Jan Keromnes <janx@linux.com>
2014-05-14 21:19:33 +0200

    WIP Geolocation Tools

---
+++ apps/system/js/devtools/geolocation_tools.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function(exports) {
+
+  /**
+   * Geolocation Tools
+   * @class GeolocationTools
+   */
+  function GeolocationTools() {
+  }
+
+  GeolocationTools.prototype = {
+  };
+
+  exports.GeolocationTools = GeolocationTools;
+
+}(window));
